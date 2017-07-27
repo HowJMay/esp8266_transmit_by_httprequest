95fe60d7e45737f94670e47b5bd87d7dc1991363
unknown <S01144@sunon.com.tw>
2017-07-27 11:10:11 +0800

    copy from web havent tested

--- app/src/main/java/com/example/s01144/esp8266_transmit_by_httprequest/MainActivity.java
+++ app/src/main/java/com/example/s01144/esp8266_transmit_by_httprequest/MainActivity.java
@@ -8,8 +8,22 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.ClientCertRequest;
 import android.widget.Button;
 import android.widget.EditText;
+
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -65,6 +79,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             new HttpRequestAsyncTask(view.getContext(), parameterValue, ipAddress, portNumber, "pin").execute();
         }
     }
+    /**
+         * Description: Send an HTTP Get request to a specified ip address and port.
+         * Also send a parameter "parameterName" with the value of "parameterValue".
+         * @param parameterValue the pin number to toggle
+         * @param ipAddress the ip address to send the request to
+         * @param portNumber the port number of the ip address
+         * @param parameterName
+         * @return The ip address' reply text, or an ERROR message is it fails to receive one
+         */
+    public String sendRequest(String parameterValue, String ipAddress, String portNumber, String parameterName){
+        String serverResponse = "ERROR";
+
+        try {
+
+            HttpClient httpClient = new DefaultHttpClient(); // create an HTTP client
+            // define the URL e.g. http://myIpaddress:myport/?pin=13 (to toggle pin 13 for example)
+            URI website = new URI("http://" + ipAddress + ":" + portNumber + "/?" + parameterName + "=" + parameterValue);
+            HttpGet getRequest = new HttpGet();// create an HTTP GET object
+            getRequest.setURI(website);// set the URL of the GET request
+            HttpResponse response = httpClient.execute(getRequest); // execute the request
+            // get the ip address server's reply
+            InputStream content = null;
+            content = response.getEntity().getContent();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(content));
+            serverResponse = bufferedReader.readLine();
+            //Close connection
+            content.close();
+
+        } catch (IOException e){
+            serverResponse = e.getMessage();
+        } catch (URISyntaxException e){
+            serverResponse = e.getMessage();
+        } catch (Exception e){
+            serverResponse = e.getMessage();
+        }
+
+        return serverResponse;
+    }
 
     private class HttpRequestAsyncTask extends AsyncTask<Void, Void, Void>{
 
@@ -81,15 +133,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                  * @param ipAddress the ip address to send the request to
                  * @param portNumber the port number of the ip address
                  */
-        public HttpRequestAsyncTask(Context context, String parameterValue, String ipAddress, String portNumber){
+        public HttpRequestAsyncTask(Context context, String parameterValue, String ipAddress, String portNumber, String parameter){
             this.context = context;
 
             alertDialog = new AlertDialog.Builder(this.context).setTitle("HTTP Response From IP Address:").setCancelable(true).create();
-            
+            this.ipAddress = ipAddress;
+            this.parameterValue = parameterValue;
+            this.portNumber = portNumber;
+            this.parameter = parameter;
         }
+        /**
+                 * Name: doInBackground
+                 * Description: Sends the request to the ip address
+                 * @param voids
+                 * @return
+                 */
         @Override
         protected Void doInBackground(Void... voids) {
+            alertDialog.setMessage("Data sent, waiting for reply from server...");
+            if (!alertDialog.isShowing()){
+                alertDialog.show();
+            }
+            requestReply = sendRequest(parameterValue, ipAddress, portNumber, parameter);
+
             return null;
+        }
+
+        /**
+                 * Name: onPostExecute
+                 * Description: This function is executed after the HTTP request returns from the ip address.
+                 * The function sets the dialog's message with the reply text from the server and display the dialog
+                 * if it's not displayed already (in case it was closed by accident);
+                 * @param aVoid void parameter
+                 */
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            alertDialog.setMessage(requestReply);
+            if (!alertDialog.isShowing()){
+                alertDialog.show();;
+            }
+        }
+        /**
+                 * Name: onPreExecute
+                 * Description: This function is executed before the HTTP request is sent to ip address.
+                 * The function will set the dialog's message and display the dialog.
+                 */
+        @Override
+        protected void onPreExecute() {
+            alertDialog.setMessage("Sending data to server, please wait...");
+            if (!alertDialog.isShowing()){
+                alertDialog.show();
+            }
         }
     }
 }
